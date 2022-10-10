8af6e8bcef74200641fb9da41739de55d0339bbf
Erivan Pereira <gasparzinho633@gmail.com>
2022-10-10 17:49:06 -0300

    custom user model

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'category',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoecommerce.wsgi.application'
 
+AUTH_USER_MODEL = 'account.Account'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
