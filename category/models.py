8ff05dc7ce1335b5c9092e2ecb2fc07d2f6e2404
Erivan Pereira <gasparzinho633@gmail.com>
2022-10-10 16:55:15 -0300

    added category model

---
+++ category/models.py
@@ -0,0 +1,20 @@
+from distutils.command.upload import upload
+from pyexpat import model
+from statistics import mode
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=255, blank=True)
+    category_image = models.ImageField(upload_to='photos/categories', blank=True)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
