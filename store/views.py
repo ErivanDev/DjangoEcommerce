b7ac983c3c96a5fc9d427e83a2856675016518e6
Erivan Pereira <gasparzinho633@gmail.com>
2022-10-12 16:37:24 -0300

    Added product infos view

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.shortcuts import get_object_or_404, render
 
 from category.models import Category
@@ -26,4 +27,13 @@ def store(request, category_slug=None):
     return render(request, 'store/store.html', context)
 
 def product_detail(request, category_slug, product_slug):
-    return render(request, 'store/product_detail.html')
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context = {
+        'single_product': single_product,
+    }
+
+    return render(request, 'store/product_detail.html', context)
