2801068a2bd6d0a98926bf94cf0d3e069171ec97
Erivan Pereira <gasparzinho633@gmail.com>
2022-10-14 17:00:56 -0300

    Added Funcionality: Added Store Pagination

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
 from itertools import product
 from unicodedata import category
+
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 from carts.models import CartItem
 from carts.views import _cart_id
 
@@ -18,13 +21,19 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.all().filter(category=categories, is_available=True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         products_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         products_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'products_count': products_count,
     }
 
